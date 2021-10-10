12e80a257a9dfe590a741c10db3309ec6af1368d
Meli <imelissajackson@yahoo.com>
2021-10-09 21:52:16 -0400

    First commit

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    private Rigidbody2D rd2d;
+    public float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       rd2d = GetComponent<Rigidbody2D>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        float hozMovement = Input.GetAxis("Horizontal");
+        float verMovement = Input.GetAxis("Vertical");
+
+        rd2d.AddForce(new Vector2(hozMovement * speed, verMovement * speed));
+
+    }
+}
